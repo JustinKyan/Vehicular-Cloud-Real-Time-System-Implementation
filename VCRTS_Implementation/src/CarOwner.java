eb8af8ba2094a0337c68df3cc8327e3c5fc1be31
JustinKyan <99352967+JustinKyan@users.noreply.github.com>
2023-02-19 22:55:55 -0500

    Add files via upload
    The VCRTS_Implementation project

---
+++ VCRTS_Implementation/src/CarOwner.java
@@ -0,0 +1,36 @@
+
+/* Project:  VCRTS: Vehicular Cloud Real Time System
+* Class: CarOwner.java 
+* Author:   Justin Kyan, Goden Liu, Andy Mathura, Tahmidul Haque, David Chang, Kevin Boukpessi 
+* Date: 2/14/2023
+* This program allows for the creation of a CarOwner Object that stores it's 
+* information including Id, Vehicle Info, and Residency Time  
+*/
+import java.util.*;
+
+public class CarOwner {
+	private int ownerID;
+	private String vehicleInfo, time;
+
+	public CarOwner(int ownerID, String vehicleInfo, String time) {
+		this.ownerID = ownerID;
+		this.vehicleInfo = vehicleInfo;
+		this.time = time;
+	}
+
+	public int getownerID() {
+		return ownerID;
+	}
+
+	public String getvehicleInfo() {
+		return vehicleInfo;
+	}
+
+	public String getTime() {
+		return time;
+	}
+
+	public String toString() {
+		return "ownerID: " + ownerID + "\nVehicle Info: " + vehicleInfo + "\nResidency Time: " + time + "\n";
+	}
+}
---
+++ VCRTS_Implementation/src/VCRTSRun.java
@@ -0,0 +1,15 @@
+
+/* Project:  VCRTS: Vehicular Cloud Real Time System
+* Class: VCRTSRun.java 
+* Author:   Justin Kyan, Goden Liu, Andy Mathura, Tahmidul Haque, David Chang, Kevin Boukpessi 
+* Date: 2/14/2023
+* This program allows for the running of a new MainGUI class 
+*/
+import java.io.FileNotFoundException;
+
+public class VCRTSRun {
+	public static void main(String[] args) throws FileNotFoundException {
+		new MainGUI("CUS 1166: VCRTS GUI for Group 1");
+	}
+
+}
