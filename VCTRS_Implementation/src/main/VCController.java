6180c288a92f6577e2ef198e0ef2e6d66fde2e15
dchan222 <99790110+dchan222@users.noreply.github.com>
2023-04-02 21:57:53 -0400

    Add files via upload

--- VCTRS_Implementation/src/main/VCController.java
+++ VCTRS_Implementation/src/main/VCController.java
@@ -1,4 +1,9 @@
 package main;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Queue;
@@ -13,6 +18,15 @@ public class VCController {
     private ArrayList<CarOwner> registeredOwners;
     private ArrayList<Job> jobsInProgress, submittedJobs, completedJobs, checkpointedJobs;
     
+    // Server implementation variables
+    static ServerSocket serverSocket;
+    static ServerSocket serverSocket2;
+    static Socket socket;
+    static Socket socket2;
+    static DataInputStream inputStream;
+    static DataOutputStream outputStream;
+    static DataInputStream inputStream2;
+    static DataOutputStream outputStream2;
 
     Queue<Job> getJobs(){ return this.jobs; }
     
@@ -37,9 +51,12 @@ public class VCController {
             current.completionTime = total;
         }
     }
-     
    
-   
+    
+    /* boolean isJobComplete() {
+    	// return true if current job @ lookup's completion time is 0. else return false.
+    }
+    */
     
     void printCurrentJobList() {
     	for(int i = 0; i < this.jobs.size(); i++) {
@@ -47,8 +64,13 @@ public class VCController {
     	}
     }
     
-
-    void addCar(Car v){ cars.add(v); } //recruit car
+    /* boolean deadlinePassed() {
+    	// return true if the current time is past the allocated deadline in "Job.java" [String at the moment, Date later]
+    	// else return false.
+    }
+    */
+    
+    void addCar(Car v){ cars.add(v); }
     
     void removeCar(ArrayList<Job> job, Car v) {
     	// Removes a vehicle from the over-all arraylist (across all registeredCars) if it exists.
@@ -65,5 +87,76 @@ public class VCController {
     	// In the event of a memory leak or any other issue, re-start.
     }
     
-    
+    public static void main(String[] args) {
+        String messageIn = "";
+        String messageOut = "";
+        String response = "";
+        boolean isRunning = false;
+        try {
+			serverSocket = new ServerSocket(3000);
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+    	try {
+			serverSocket2 = new ServerSocket(3001);
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+    	
+        try {
+            isRunning = true;
+            ServerGUI serverGUI = new ServerGUI();
+            
+            serverGUI.acceptButton.setVisible(false);
+        	serverGUI.rejectButton.setVisible(false);
+        	
+        	serverGUI.incomingRequest.setText("[WELCOME]: This is Carboard's server for handling job requests.\n");
+            serverGUI.incomingRequest.append("Waiting for a request...\n");
+            
+            
+            socket = serverSocket.accept();
+            serverGUI.incomingRequest.append("[NOTICE] User connection detected.");
+            inputStream = new DataInputStream(socket.getInputStream());
+            outputStream = new DataOutputStream(socket.getOutputStream());
+            socket2 = serverSocket2.accept();
+            
+            while (isRunning) {
+            	serverGUI.incomingRequest.append("\nAwaiting Message\n");
+                 	
+            	serverGUI.messageIn = inputStream.readUTF();
+            	
+            	serverGUI.incomingRequest.setText("Message Received\n");
+            	
+            	serverGUI.acceptButton.setVisible(true);
+            	serverGUI.rejectButton.setVisible(true);
+            	
+            	serverGUI.incomingRequest.append(serverGUI.messageIn);
+            	
+            	serverGUI.incomingRequest.append("\nDo You Accept or Reject this Submission?\n");
+            	
+                
+                inputStream2 = new DataInputStream(socket2.getInputStream());
+        		outputStream2 = new DataOutputStream(socket2.getOutputStream());
+        		
+        		serverGUI.messageOut = inputStream2.readUTF();
+        		
+        		serverGUI.acceptButton.setVisible(false);
+            	serverGUI.rejectButton.setVisible(false);
+        		
+        		if(serverGUI.messageOut.equals("Accept")) {
+        			serverGUI.incomingRequest.append("\n[NOTICE] Submission accepted.\n");
+                    outputStream.writeUTF(serverGUI.messageOut);
+                }
+        		else if(serverGUI.messageOut.equals("Reject")) {
+        			serverGUI.incomingRequest.append("\n[NOTICE] Submission rejected.\n");
+                	outputStream.writeUTF(serverGUI.messageOut);
+                }
+        		
+            } 
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
